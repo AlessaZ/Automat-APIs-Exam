528d61a4fcaaefd95c099e97f15737893939f6df
AlessaZ <azmoralesm@pucp.edu.pe>
2024-10-02 16:00:06 -0500

    Cambios finales - Modificación de archivo PetStoreCreateOrderStep

--- src/test/java/com/everis/base/steps/PetStoreCreateOrderStep.java
+++ src/test/java/com/everis/base/steps/PetStoreCreateOrderStep.java
@@ -3,14 +3,14 @@ package com.everis.base.steps;
 import com.everis.base.models.Order;
 import io.restassured.response.Response;
 
-
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PetStoreCreateOrderStep {
 
-    private String URL_BASE = "https://petstore.swagger.io/v2" ;
+    private static final String URL_BASE = "https://petstore.swagger.io/v2";
     private Response response;
-    private int codigoRespuesta;
     private Order orderResponse;
 
     public void crearOrden(int id, int petId, int quantity) {
@@ -21,31 +21,20 @@ public class PetStoreCreateOrderStep {
                 .header("Content-Type", "application/json")
                 .body(order)
                 .when()
-                .post("/store/order")
-                .then()
-                .statusCode(200)
-                .extract()
-                .response();
+                .post("/store/order");
 
-        codigoRespuesta = response.getStatusCode();
         orderResponse = response.as(Order.class);
 
-        System.out.println("Creado: ID "+ orderResponse.getId());
-        System.out.println("Creado: Pet ID "+ orderResponse.getPetId());
-        System.out.println("Creado: Quantity "+ orderResponse.getQuantity());
-
-
+        System.out.println("Creado: ID " + orderResponse.getId());
+        System.out.println("Creado: Pet ID " + orderResponse.getPetId());
+        System.out.println("Creado: Quantity " + orderResponse.getQuantity());
     }
 
     public void validarCodigoRespuesta(int code) {
-        if(code != codigoRespuesta){
-            throw new AssertionError("Código esperado: "  + code + "Código obtenido: " + codigoRespuesta);
-        }
-
+        assertThat(response.getStatusCode(), is(code));
     }
 
-    public Order obtenerRespuestadeOrder(){
+    public Order obtenerRespuestadeOrder() {
         return orderResponse;
     }
-
 }
